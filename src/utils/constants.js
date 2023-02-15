ec714d5897378dc84cdfdf4c70601c3954d03e38
Rishabh <rishabh.9agarwal@gmail.com>
2023-02-15 14:49:08 +0530

    fixed demo URLs

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -16,7 +16,7 @@ export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
 
 export const categories = [
   { name: 'New', icon: <HomeIcon />, },
-  { name: 'JS Mastery', icon: <CodeIcon />, },
+  { name: 'Chelsea FC', icon: <CodeIcon />, },
   { name: 'Coding', icon: <CodeIcon />, },
   { name: 'ReactJS', icon: <CodeIcon />, },
   { name: 'NextJS', icon: <CodeIcon />, },
@@ -34,9 +34,9 @@ export const categories = [
   { name: 'Crypto', icon: <DeveloperModeIcon />, },
 ];
 
-export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
-export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
-export const demoVideoUrl = '/video/GDa8kZLNhJ4';
-export const demoChannelTitle = 'JavaScript Mastery';
-export const demoVideoTitle = 'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
+export const demoThumbnailUrl = 'https://img.youtube.com/vi/FZUcpVmEHuk/maxresdefault.jpg';
+export const demoChannelUrl = '/channel/UCU2PacFf99vhb3hNiYDmxww';
+export const demoVideoUrl = '/video/FZUcpVmEHuk';
+export const demoChannelTitle = 'Chelsea FC';
+export const demoVideoTitle = 'still a piece of garbage';
 export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
